b78006993b95afad98d5b8d7f8bc6d822bd69cd1
gosueep <eugin03@gmail.com>
2022-10-31 17:42:03 -0600

    ready to abuse isengard

--- WindowMaker.py
+++ WindowMaker.py
@@ -1,4 +1,5 @@
 import os
+from multiprocessing import Process
 
 import numpy as np
 from common import readData
@@ -32,27 +33,34 @@ def getWindows(X, y, WINDOW_SIZE=100, STEP_SIZE=1):
     return windows, y_values
 
 
-def makeWindows(WINDOW_SIZE, STEP_SIZE, recreate=False):
+def makeWindows(WINDOW_SIZE, STEP_SIZE, recreate=True):
+    procs = []
     for i in range(0, 10):
         if recreate or not os.path.isfile(f'windows/testsplit{i}.pkl'):
             X, y = readData(f'data/testsplit{i}.csv')
             windows, y_values = getWindows(X, y, WINDOW_SIZE, STEP_SIZE)
             joblib.dump((windows, y_values), f'windows/testsplit{i}.pkl')
 
+            # parallelize downloads - SO MUCH FASTER
+            proc = Process(target=getWindows, args=(X, y, WINDOW_SIZE, STEP_SIZE))
+            procs.append(proc)
+            proc.start()
+            proc.join()
+
 
 if __name__ == '__main__':
     WINDOW_SIZE = 10000
-    STEP_SIZE = 1000
+    STEP_SIZE = 100
 
-    # makeWindows(WINDOW_SIZE, STEP_SIZE)
+    makeWindows(WINDOW_SIZE, STEP_SIZE)
 
-    X, y = readData('./data/testsplit0.csv')
-    print('READ IN DATA')
-    windows, y_values = getWindows(X, y, WINDOW_SIZE, STEP_SIZE)
+    # X, y = readData('./data/testsplit0.csv')
+    # print('READ IN DATA')
+    # windows, y_values = getWindows(X, y, WINDOW_SIZE, STEP_SIZE)
+    #
+    # print(f'Total Windows: {len(windows)}')
+    # joblib.dump((windows, y_values), 'windows/testsplit0.pkl')
 
-    print(f'Total Windows: {len(windows)}')
-    joblib.dump((windows, y_values), 'windows/testsplit0.pkl')
 
-
-
+# ~ 3-4 mins for 500 step size
 
