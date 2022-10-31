f26ded9827f3e9d1d331ce2b253c02910155d6d6
gosueep <eugin03@gmail.com>
2022-10-31 17:08:21 -0600

    step size 100 .69 R^2

--- WindowMaker.py
+++ WindowMaker.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 from common import readData
 import joblib
@@ -30,25 +32,25 @@ def getWindows(X, y, WINDOW_SIZE=100, STEP_SIZE=1):
     return windows, y_values
 
 
-# def makeWindows(filename):
-#     X, y = readData('plot.csv')
-#     windows = getWindows(X, 100)
-#     print(windows)
-#
-#     joblib.dump(windows, 'windows/plot')
+def makeWindows(WINDOW_SIZE, STEP_SIZE, recreate=False):
+    for i in range(0, 10):
+        if recreate or not os.path.isfile(f'windows/testsplit{i}.pkl'):
+            X, y = readData(f'data/testsplit{i}.csv')
+            windows, y_values = getWindows(X, y, WINDOW_SIZE, STEP_SIZE)
+            joblib.dump((windows, y_values), f'windows/testsplit{i}.pkl')
 
 
 if __name__ == '__main__':
+    WINDOW_SIZE = 10000
+    STEP_SIZE = 1000
+
+    # makeWindows(WINDOW_SIZE, STEP_SIZE)
+
     X, y = readData('./data/testsplit0.csv')
     print('READ IN DATA')
-    # X, y = readData('plot.csv')
-    WINDOW_SIZE = 10000
-    STEP_SIZE = 5000
     windows, y_values = getWindows(X, y, WINDOW_SIZE, STEP_SIZE)
 
-    # joblib.dump([windows, y[:-WINDOW_SIZE]], 'windows/plot.pkl')
-    print(len(windows))
-    print(len(y[:-WINDOW_SIZE]))
+    print(f'Total Windows: {len(windows)}')
     joblib.dump((windows, y_values), 'windows/testsplit0.pkl')
 
 
